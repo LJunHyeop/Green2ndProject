0a71a25e4593121ef51fcb558036a1d6aa03e438
Administrator <wltngus200@gmail.com>
2024-07-01 14:36:01 +0900

    0701 socialLogin

--- src/main/java/com/green/fefu/security/JwtAuthenticationFilter.java
+++ src/main/java/com/green/fefu/security/JwtAuthenticationFilter.java
@@ -1,14 +1,14 @@
-//package com.green.fefu.security;
-//
-//import jakarta.servlet.FilterChain;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.filter.OncePerRequestFilter;
-//@Component
-//public class JwtAuthenticationFilter extends OncePerRequestFilter {
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain){
-//        String token;
-//    }
-//}
+package com.green.fefu.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain){
+        String token;
+    }
+}
