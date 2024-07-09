8a0f48a6614b7731d090f572080af304200d0f60
Administrator <dlwnsguq2002@gmail.com>
2024-07-09 10:07:42 +0900

    2nd 프로젝트

---
+++ src/main/java/com/green/fefu/score/model/JshGetRes.java
@@ -0,0 +1,12 @@
+package com.green.fefu.score.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JshGetRes {
+    //과목 이름
+    private String name;
+    private int score;
+}
