7a1d27e121ca4ef91f8a2a68a2be5e10990d7699
Administrator <wltngus200@gmail.com>
2024-07-09 12:20:13 +0900

    0709 선생님 학급 찾기 메소드 생성중

--- src/main/java/com/green/fefu/notice/NoticeServiceImpl.java
+++ src/main/java/com/green/fefu/notice/NoticeServiceImpl.java
@@ -1,7 +1,11 @@
 package com.green.fefu.notice;
 
+import com.green.fefu.common.AppProperties;
+import com.green.fefu.common.CookieUtils;
 import com.green.fefu.notice.model.*;
+import com.green.fefu.security.AuthenticationFacade;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,9 +14,15 @@ import java.util.List;
 @RequiredArgsConstructor
 public class NoticeServiceImpl implements NoticeService{
     private final NoticeMapper mapper;
+    private final PasswordEncoder passwordEncoder;
+    private CookieUtils cookieUtils;
+    private final AuthenticationFacade authenticationFacade; //PK값을 제공(getLoginUserId();
+    private final AppProperties appProperties;
 
 
     public int postNotice(PostNoticeReq p){
+        p.setTeaId(authenticationFacade.getLoginUserId());
+        p.setClassId(mapper.teacherHomeroom(p.getTeaId()));
         return mapper.postNotice(p);
     }
     public List<GetNoticeRes> getNotice(GetNoticeReq p){
@@ -25,4 +35,5 @@ public class NoticeServiceImpl implements NoticeService{
     public int deleteNotice(DeleteNoticeReq p){
         return mapper.deleteNotice(p);
     }
+
 }
