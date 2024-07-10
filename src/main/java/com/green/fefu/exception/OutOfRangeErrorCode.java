b18b96bab854b3894ada41d026cc03ec3db4057b
Administrator <wltngus200@gmail.com>
2024-07-10 20:12:29 +0900

    0710 Exception-메모 남김

--- src/main/java/com/green/fefu/exception/OutOfRangeErrorCode.java
+++ src/main/java/com/green/fefu/exception/OutOfRangeErrorCode.java
@@ -14,6 +14,8 @@ public enum OutOfRangeErrorCode implements ErrorCode{
     //권한 없음, 토큰 만료, 뭐가 또 있지........?!
     //2학기 아직 시작 안 했는데 2학기 조회라던가 "해당학기의 정보를 조회할 수 없습니다."
     //아이디 비번 일치, 회원정보, 비밀번호나 다른거 정규식,
+    //점수 값 이상함 101점이라거나, -1점
+    //블랙리스트 올라감 ㅎ
 
     private final HttpStatus httpStatus;
     private final String message;
