308f08b49c97f9f80fff4e3707909a98ecf2cfe1
17-012 <ckdgus1373@naver.com>
2024-07-10 16:01:14 +0900

    2024-07-09

--- src/main/java/com/green/fefu/student/model/req/studentAdvanceGradeReq.java
+++ src/main/java/com/green/fefu/student/model/req/studentAdvanceGradeReq.java
@@ -1,6 +1,7 @@
 package com.green.fefu.student.model.req;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,7 +10,9 @@ import lombok.ToString;
 @Setter
 @ToString
 public class studentAdvanceGradeReq {
+    @Schema(example = "1", description = "학생 pk", required = true)
     private String studentPk;
+    @Schema(example = "10101", description = "진학 하는 학년/반/번호", required = true)
     private String grade;
 
     @JsonIgnore
