e068ce6324c078d69627f5b0a0ca4a919c8b89eb
Administrator <wltngus200@gmail.com>
2024-07-11 11:20:35 +0900

    0710 Notice Controller @Operation 수정

--- src/main/java/com/green/fefu/notice/NoticeControllerImpl.java
+++ src/main/java/com/green/fefu/notice/NoticeControllerImpl.java
@@ -22,7 +22,11 @@ public class NoticeControllerImpl implements NoticeController{
 
 
     @PostMapping("")
-    @Operation(summary = "알림장 작성", description = "<strong>각 학급의 알림장</strong>")
+    @Operation(summary = "알림장 작성",
+            description = "<strong> 변수명 : tea_id </strong> <p> 선생님 PK ex)15 </p>" +
+                    "<strong> 변수명 : title </strong> <p> 알림장 제목 ex)금요일 생일파티 공지 </p>" +
+                    "<strong> 변수명 : content </strong> <p> 알림장 내용 ex)과자 1봉 가져오기 </p>" +
+                    "<strong> 변수명 : state </strong> <p> 알림장 항목(1 : 알림장 / 2 : 준비물)  ex)1 </p>")
     public ResultDto<Integer> postNotice(@RequestBody PostNoticeReq p){
         int result = service.postNotice(p);
         return ResultDto.<Integer>builder()
@@ -33,7 +37,9 @@ public class NoticeControllerImpl implements NoticeController{
     }
 
     @GetMapping("")
-    @Operation(summary = "알림장 조회", description = "<strong>반 별 알림장 조회</strong>")
+    @Operation(summary = "알림장 조회",
+            description = "<strong> 변수명 : class_id </strong> <p> 학급 PK ex) 101 </p>" +
+                    "<strong> 변수명 : state </strong> <p> 알림장 항목(1 : 알림장 / 2 : 준비물)  ex)1 </p>")
     public ResultDto<List<GetNoticeRes>> getNotice(@ModelAttribute @ParameterObject GetNoticeReq p){
         List<GetNoticeRes> list=service.getNotice(p);
         return ResultDto.<List<GetNoticeRes>>builder()
@@ -44,7 +50,11 @@ public class NoticeControllerImpl implements NoticeController{
     }
 
     @PutMapping("") //구현 예정
-    @Operation(summary = "알림장 수정", description = "<strong>알림장 수정</strong>")
+    @Operation(summary = "알림장 수정",
+            description = "<strong> 변수명 : notice_id </strong> <p> 알림장 PK ex)1 </p>" +
+                    "<strong> 변수명 : tea_id </strong> <p> 선생님 PK ex)6 </p>" +
+                    "<strong> 변수명 : title </strong> <p> 수정할 제목 ex)수요일 운동회  </p>" +
+                    "<strong> 변수명 : content </strong> <p> 수정할 내용 ex)우천시 실내체험으로 변경 </p>")
     public ResultDto<Integer> putNotice(@RequestBody PutNoticeReq p){
         int result=service.putNotice(p);
         return ResultDto.<Integer>builder()
@@ -55,7 +65,9 @@ public class NoticeControllerImpl implements NoticeController{
     }
 
     @DeleteMapping("")
-    @Operation(summary = "알림장 삭제", description = "<strong>필요없는 알림장 삭제</strong>")
+    @Operation(summary = "알림장 삭제",
+            description = "<strong> 변수명 : notice_id </strong> <p> 알림장 PK ex)1 </p>" +
+                    "<strong> 변수명 : tea_id </strong> <p> 선생님 PK ex)6 </p>")
     public ResultDto<Integer> deleteNotice(@ModelAttribute @ParameterObject DeleteNoticeReq p){
         int result=service.deleteNotice(p);
         return ResultDto.<Integer>builder()
