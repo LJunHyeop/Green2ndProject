d317084fbf141d53e408d5b2538507826ccaf6fb
Administrator <wltngus200@gmail.com>
2024-07-11 17:34:54 +0900

    0710 Notice Mapper TDD 작업 중

--- src/main/java/com/green/fefu/notice/NoticeControllerImpl.java
+++ src/main/java/com/green/fefu/notice/NoticeControllerImpl.java
@@ -25,7 +25,7 @@ public class NoticeControllerImpl implements NoticeController{
 
     @PostMapping("")
     @Operation(summary = "알림장 작성",
-            description = "<strong> 변수명 : tea_id </strong> <p> 선생님 PK ex)15 </p>" +
+            description =
                     "<strong> 변수명 : title </strong> <p> 알림장 제목 ex)금요일 생일파티 공지 </p>" +
                     "<strong> 변수명 : content </strong> <p> 알림장 내용 ex)과자 1봉 가져오기 </p>" +
                     "<strong> 변수명 : state </strong> <p> 알림장 항목(1 : 알림장 / 2 : 준비물)  ex)1 </p>")
@@ -41,8 +41,7 @@ public class NoticeControllerImpl implements NoticeController{
 
     @GetMapping("")
     @Operation(summary = "알림장 조회",
-            description = "<strong> 변수명 : class_id </strong> <p> 학급 PK ex) 101 </p>" +
-                    "<strong> 변수명 : state </strong> <p> 알림장 항목(1 : 알림장 / 2 : 준비물)  ex)1 </p>")
+            description = "<strong> 변수명 : state </strong> <p> 알림장 항목(1 : 알림장 / 2 : 준비물)  ex)1 </p>")
     public ResultDto<List<GetNoticeRes>> getNotice(@ModelAttribute @ParameterObject GetNoticeReq p){
         List<GetNoticeRes> list=service.getNotice(p);
         return ResultDto.<List<GetNoticeRes>>builder()
@@ -55,7 +54,6 @@ public class NoticeControllerImpl implements NoticeController{
     @PutMapping("") //구현 예정
     @Operation(summary = "알림장 수정",
             description = "<strong> 변수명 : notice_id </strong> <p> 알림장 PK ex)1 </p>" +
-                    "<strong> 변수명 : tea_id </strong> <p> 선생님 PK ex)6 </p>" +
                     "<strong> 변수명 : title </strong> <p> 수정할 제목 ex)수요일 운동회  </p>" +
                     "<strong> 변수명 : content </strong> <p> 수정할 내용 ex)우천시 실내체험으로 변경 </p>")
     @PreAuthorize("hasRole('TEAHCER')")
@@ -70,8 +68,7 @@ public class NoticeControllerImpl implements NoticeController{
 
     @DeleteMapping("")
     @Operation(summary = "알림장 삭제",
-            description = "<strong> 변수명 : notice_id </strong> <p> 알림장 PK ex)1 </p>" +
-                    "<strong> 변수명 : tea_id </strong> <p> 선생님 PK ex)6 </p>")
+            description = "<strong> 변수명 : notice_id </strong> <p> 알림장 PK ex)1 </p>" )
     @PreAuthorize("hasRole('TEAHCER')")
     public ResultDto<Integer> deleteNotice(@ModelAttribute @ParameterObject DeleteNoticeReq p){
         int result=service.deleteNotice(p);
