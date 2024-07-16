985967a46b250bc0aee5949b788bdf09f06d55da
tg03067 <163490444+tg03067@users.noreply.github.com>
2024-07-16 15:36:45 +0900

    LJM second_project parents 비밀번호 조회 수정, token 재발급, 자녀정보 조회시 사진 조회

--- src/test/java/com/green/fefu/parents/ParentsUserControllerTest.java
+++ src/test/java/com/green/fefu/parents/ParentsUserControllerTest.java
@@ -2,13 +2,13 @@ package com.green.fefu.parents;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.green.fefu.CharEncodingConfiguration;
+import com.green.fefu.common.AppProperties;
 import com.green.fefu.parents.model.PostParentsUserReq;
 import com.green.fefu.security.SecurityConfiguration;
 import com.green.fefu.security.jwt.JwtTokenProviderV2;
 import com.green.fefu.security.oauth2.OAuth2AuthenticationFailureHandler;
 import com.green.fefu.security.oauth2.OAuth2AuthenticationSuccessHandler;
 import com.green.fefu.sms.SmsService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,54 +16,66 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest({ParentsUserControllerImpl.class})
-@Import({CharEncodingConfiguration.class, SecurityConfiguration.class})
+@Import({CharEncodingConfiguration.class})
 @AutoConfigureMockMvc(addFilters = false)  // Spring Security 필터 비활성화
 @WebAppConfiguration
 class ParentsUserControllerTest {
-    @Autowired private ObjectMapper objectMapper;
+    @Autowired private ObjectMapper om;
     @Autowired private MockMvc mockMvc;
     @MockBean private ParentsUserServiceImpl service;
     @MockBean private JwtTokenProviderV2 tokenProvider;
     @MockBean private SmsService smsService;
-    @MockBean private OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
-    @MockBean private UserDetailsService userDetailsService;
-    @MockBean private OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
-    @MockBean private com.green.fefu.security.oauth2.OAuth2AuthenticationRequestBasedOnCookieRepository oAuth2AuthenticationRequestBasedOnCookieRepository;
-    @MockBean private com.green.fefu.security.oauth2.MyOAuth2UserService myOAuth2UserService;
+
     private final String BASE_URL = "/api/user/parents";
 
     @Test
     @DisplayName("회원가입 TDD")
     void testPostParents() throws Exception {
         // Given
-        PostParentsUserReq req = new PostParentsUserReq();
+        PostParentsUserReq req = new PostParentsUserReq() ;
+        req.setParentsId(1) ;
+        req.setStudentPk(1) ;
+        req.setNm("길동") ;
+        req.setEmail("rffrfr@gmail.com") ;
+        req.setConnet("부") ;
+        req.setPhone("010-1235-4567") ;
+        req.setUid("parent745") ;
+        req.setUpw("Test1234!@#$");
+
         // 설정 필요한 필드들 추가
-        given(service.postParentsUser(any(PostParentsUserReq.class))).willReturn(1);
+        given(service.postParentsUser(any(PostParentsUserReq.class))).willReturn(1) ;
+        String json = om.writeValueAsString(req) ;
+
+        Map<String, Object> expectedMap = new HashMap();
+        expectedMap.put("statusCode", HttpStatus.OK);
+        expectedMap.put("resultMsg", HttpStatus.OK.toString());
+        expectedMap.put("resultData", 1);
+        String expectedJson = om.writeValueAsString(expectedMap);
 
         // When & Then
         mockMvc.perform(MockMvcRequestBuilders.post("/api/user/parents/sign-up")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"field1\":\"value1\",\"field2\":\"value2\"}")) // JSON 요청 바디
+                        .content(expectedJson)) // JSON 요청 바디
                 .andExpect(status().isOk())
                 .andExpect(content().string("1"));
+        verify(service).postParentsUser(req) ;
     }
 
     @Test
