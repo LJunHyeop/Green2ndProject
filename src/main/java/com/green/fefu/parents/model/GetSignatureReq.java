a513a4c5232521d1fa3c425398a6f3bd2173ee41
tg03067 <163490444+tg03067@users.noreply.github.com>
2024-07-18 18:43:18 +0900

    LJM second_project parents 부분 수정

---
+++ src/main/java/com/green/fefu/parents/model/GetSignatureReq.java
@@ -0,0 +1,10 @@
+package com.green.fefu.parents.model;
+
+import lombok.Data;
+
+@Data
+public class GetSignatureReq {
+    private long stuId ;
+    private int year ;
+    private int semester ;
+}
---
+++ src/main/java/com/green/fefu/parents/model/GetSignatureRes.java
@@ -0,0 +1,10 @@
+package com.green.fefu.parents.model;
+
+import lombok.Data;
+
+@Data
+public class GetSignatureRes {
+    private long stuId ;
+    private int year ;
+    private int semester ;
+}
