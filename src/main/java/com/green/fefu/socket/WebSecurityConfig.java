a3b678b4934a5aa3122403d24412b1819575d212
LJunHyeop <163490491+LJunHyeop@users.noreply.github.com>
2024-07-30 18:11:50 +0900

    2nd 프로젝트

--- src/main/java/com/green/fefu/socket/WebSecurityConfig.java
+++ src/main/java/com/green/fefu/socket/WebSecurityConfig.java
@@ -7,17 +7,17 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig  {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .httpBasic(https -> https.disable())
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/ws/**").
-                        permitAll().anyRequest().permitAll()
-                ).build();
-    }
-}
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig  {
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception {
+//        return http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .httpBasic(https -> https.disable())
+//                .csrf(csrf -> csrf.disable())
+//                .authorizeHttpRequests(auth -> auth.requestMatchers("/ws/**").
+//                        permitAll().anyRequest().permitAll()
+//                ).build();
+//    }
+//}
