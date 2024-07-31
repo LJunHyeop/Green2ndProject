f6bcf61b3f5c7487e20870af6ac1a7e8acc086f4
Administrator <wltngus200@gmail.com>
2024-07-31 16:35:38 +0900

    0731 import.sql 생성 테스트용 시험문제

--- src/main/java/com/green/fefu/online/OnlineKoreanController.java
+++ src/main/java/com/green/fefu/online/OnlineKoreanController.java
@@ -1,12 +1,18 @@
 package com.green.fefu.online;
 
 import com.green.fefu.common.model.ResultDto;
+import com.green.fefu.online.model.GetKoreanQuestionReq;
+import com.green.fefu.online.model.PostOnlineQuestionReq;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -14,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/online")
 public class OnlineKoreanController {
     @PostMapping
-    public ResultDto<Integer> PostKoreanQuestion(){
+    public ResultDto<Integer> PostKoreanQuestion(PostOnlineQuestionReq p){
         int result=1;
         return ResultDto.<Integer>builder()
                 .statusCode(HttpStatus.OK)
@@ -22,4 +28,13 @@ public class OnlineKoreanController {
                 .resultMsg(HttpStatus.OK.toString())
                 .build();
     }
+
+    @GetMapping
+    public ResultDto<List<GetKoreanQuestionReq>> GetKoreanQuestion(){
+        List<GetKoreanQuestionReq> result=new ArrayList<>();
+        return ResultDto.<List<GetKoreanQuestionReq>>builder()
+                .result(result)
+                .build();
+    }
+
 }
