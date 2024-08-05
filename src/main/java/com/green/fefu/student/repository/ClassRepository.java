614f3d94bc864588b5f7a54728500a3ba31ce9c6
17-012 <ckdgus1373@naver.com>
2024-08-05 15:49:50 +0900

    2024-07-11

--- src/main/java/com/green/fefu/student/repository/ClassRepository.java
+++ src/main/java/com/green/fefu/student/repository/ClassRepository.java
@@ -1,8 +1,11 @@
 package com.green.fefu.student.repository;
 
 import com.green.fefu.entity.Class;
+import com.green.fefu.entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ClassRepository extends JpaRepository<Class,Long> {
+    @Query("select cl from Class cl where cl.teaId = :teaId")
     Class getReferenceByTeaId(Long teaId);
 }
