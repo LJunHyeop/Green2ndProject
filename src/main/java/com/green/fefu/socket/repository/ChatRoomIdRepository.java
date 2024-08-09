3cf9ebbb466bdc25c13bf0a20da39a095dad7c81
LJunHyeop <163490491+LJunHyeop@users.noreply.github.com>
2024-08-09 17:53:53 +0900

    Delete src/main/java/com/green/fefu/socket directory

--- src/main/java/com/green/fefu/socket/repository/ChatRoomIdRepository.java
+++
@@ -1,7 +0,0 @@
-package com.green.fefu.socket.repository;
-
-import com.green.fefu.entity.ChatRoomId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatRoomIdRepository extends JpaRepository<ChatRoomId, Long> {
-}
