5963bcdce1b1f70b1d48a5b52bcab2ef9018782d
17-012 <ckdgus1373@naver.com>
2024-08-14 12:40:05 +0900

    2024-08-08

--- src/main/java/com/green/fefu/student/repository/StudentClassRepository.java
+++ src/main/java/com/green/fefu/student/repository/StudentClassRepository.java
@@ -11,4 +11,6 @@ public interface StudentClassRepository extends JpaRepository<StudentClass, Inte
     Long countByClassId(Class classId);
     @Query("select sc from StudentClass sc where sc.classId.classId = :classId and sc.stuId.stuId = :stuId")
     StudentClass findByClassIdAndStuId(Integer classId, Long stuId);
+    @Query("select sc from StudentClass sc where sc.classId.classId = :classId order by sc.scId desc limit 1")
+    StudentClass findTopByClassIdOrderByScId(Integer classId);
 }
