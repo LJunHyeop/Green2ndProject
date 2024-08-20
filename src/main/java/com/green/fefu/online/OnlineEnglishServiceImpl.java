27849db3c8c89afc9f34b1645247f8ef7f86b995
Administrator <wltngus200@gmail.com>
2024-08-20 12:32:03 +0900

    0820 수정

--- src/main/java/com/green/fefu/online/OnlineEnglishServiceImpl.java
+++ src/main/java/com/green/fefu/online/OnlineEnglishServiceImpl.java
@@ -111,9 +111,10 @@ public class OnlineEnglishServiceImpl {
         //사진 처리
         if (pic != null || !pic.isEmpty()) {
             try {
+                log.info("{}",entEnglishListening.getListeningPk());
                 String path = String.format("onlineEngLis/%s",entEnglishListening.getListeningPk());
                 customFileUtils.makeFolders(path);
-                String target = String.format("%s/%s", path, pic);
+                String target = String.format("%s/%s", path, picName);
                 customFileUtils.transferTo(pic, target);
             } catch (Exception e) {
                 e.printStackTrace();
